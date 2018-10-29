bcf069c0531a34f3b5cc49756629c573893bde8d
K Aravind Srinivas <k.aravindsrinivas@gmail.com>
2018-10-29 16:25:58 +0530

    Update FlaskTest1.py

--- FlaskTest1.py
+++ FlaskTest1.py
@@ -22,8 +22,8 @@ def form():
 
 @app.route('/recd',methods=['GET','POST'])
 def recd():
-    if request.method=='POST' or request.method=='GET':
-        x=request.form['name']
+    if request.method=='POST': 
+        x = request.args.get('name');
         x=' '.join(x.split());
         x=x.lower();
         x=re.sub('[^A-Za-z0-9]+', ' ', x)
